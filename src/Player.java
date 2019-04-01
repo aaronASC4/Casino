aa423f14239255f63039d611d0f88e96a8719304
aaronASC4 <aarongraves07@gmail.com>
2019-03-31 22:46:42 -0400

    not needed

--- src/Player.java
+++ src/Player.java
@@ -1,28 +1,28 @@
-public interface Player {
+// public interface Player {
 
-    // Player hand as an array.
+//     // Player hand as an array.
 
-    // Player name as a String.
+//     // Player name as a String.
 
-    /**
-     * What will happen at start of a player's turn.
-     */
-    void gameStartup();
+//     /**
+//      * What will happen at start of a player's turn.
+//      */
+//     void gameStartup();
 
-    /**
-     * Generate a card and add it to the player's hand.
-     */
-    void drawCard();
+//     /**
+//      * Generate a card and add it to the player's hand.
+//      */
+//     void drawCard();
 
-    /**
-     * Ask player if (s)he wants to hit or stay, returns a boolean on the choice of staying.
-     * @return 
-     */
-    boolean wantToStay();
+//     /**
+//      * Ask player if (s)he wants to hit or stay, returns a boolean on the choice of staying.
+//      * @return 
+//      */
+//     boolean wantToStay();
 
-    /**
-     * Retrieves the total of the player's hand.
-     * @return 
-     */
-    int getTotal();
-}
+//     /**
+//      * Retrieves the total of the player's hand.
+//      * @return 
+//      */
+//     int getTotal();
+// }
