b481cf11196c9419e3772239146f4e38b74f9ca0
aaronASC4 <aarongraves07@gmail.com>
2019-03-31 22:42:07 -0400

    fixed

--- src/Card.java
+++ src/Card.java
@@ -1,71 +1,65 @@
-public class Card {
-	private Suit suit;
-	private CardRank cardRank;
-	
-public enum Suit {
-	   Clubs,
-	   Diamonds,
-	   Hearts,
-	   Spades
+public class Card implements Comparable<Card> {
+
+    enum ranks {
+        ACE(1),
+        TWO(2),
+        THREE(3),
+        FOUR(4),
+        FIVE(5),
+        SIX(6),
+        SEVEN(7),
+        EIGHT(8),
+        NINE(9),
+        TEN(10),
+        JACK(10),
+        QUEEN(10),
+        KING(10);
+
+        private final int value;
+
+        ranks(final int value) {
+            this.value = value;
+        }
+
+        public int getValue() { return value; }
+    }
+    enum suits {
+        HEARTS,
+        DIAMONDS,
+        SPADES,
+        CLUBS
+    }
+
+    private ranks cardRank;
+    private suits cardSuit;
+    private int pointValue;
+
+    Card(ranks rank, suits suit) {
+        this.cardRank = rank;
+        this.cardSuit = suit;
+        this.pointValue = rank.getValue();
+    }
+
+    Card(ranks rank, suits suit, int pointValue) {
+        this(rank, suit);
+        this.pointValue = pointValue;
+    }
+
+
+    @Override
+    public int compareTo(Card card) {
+        return this.pointValue - card.getPointValue();
+    }
+
+    public suits getCardSuit() {
+        return cardSuit;
+    }
+
+    public ranks getCardRank() {
+        return cardRank;
+    }
+
+    public int getPointValue() {
+        return pointValue;
+    }
 }
-/*---*/
-/* Enum for Card Rankings/Number values */
-public enum CardRank {
-	Ace(1),
-	Two(2),
-	Three(3),
-	Four(4),
-	Five(5),
-	Six(6),
-	Seven(7),
-	Eight(8),
-	Nine(9),
-	Ten(10),
-	Jack(11),
-	Queen(12),
-	King(13);
-	
-	private int cardRank;
-	
-	private CardRank (int rank) {
-		this.cardRank = rank;
-	}
-	
-	public int getCardRank() {
-		return cardRank;
-	}
-} /* Enum for Card Rankings/Number ends */
-
-/*Constructors*/
-	public Card (CardRank cardRank, Suit suit) {
-		this.cardRank = cardRank;
-		this.suit = suit;
-	}
-	  public Suit getSuit()
-	  {
-	    return suit;
-	  }
-	 
-	  public void setSuit(Suit suit)
-	  {
-		  this.suit = suit;
-	  }
-	 
-	  public CardRank getCardRank()
-	  {
-	    return cardRank;
-	  }
-	 
-	  public void setCardRank(CardRank cardRank)
-	  {
-	    this.cardRank = cardRank;
-	  }
-	  
-	  public String toString() 
-	  {
-		 return cardRank+ " of "+suit;
-	  }
-	
-
-
-}	
